32545ecdca1af61116ba4d5fe2b66004e46785a7
cypark728 <cypark448@gmail.com>
2025-03-16 19:17:59 +0900

    2025-03-16

--- LINKER/src/main/java/com/LINKER/member/model/MemberDAO.java
+++ LINKER/src/main/java/com/LINKER/member/model/MemberDAO.java
@@ -16,8 +16,8 @@ public class MemberDAO {
 		return instance;
 	}
 	
-	// private String url = "jdbc:oracle:thin:@localhost:1521:xe"; // 테스트용 로컬
-	private String url = "jdbc:oracle:thin:@172.30.1.58:1521:xe";
+	private String url = "jdbc:oracle:thin:@localhost:1521:xe"; // 테스트용 로컬
+	//private String url = "jdbc:oracle:thin:@172.30.1.58:1521:xe";
 	private String uid = "LINKER";
 	private String upw = "LINKER";
 	
--- LINKER/src/main/java/com/LINKER/request/model/RequestDAO.java
+++ LINKER/src/main/java/com/LINKER/request/model/RequestDAO.java
@@ -11,8 +11,8 @@ public class RequestDAO {
 		return instance;
 	}
 	
-	// private String url = "jdbc:oracle:thin:@localhost:1521:xe"; // 테스트용 로컬
-	private String url = "jdbc:oracle:thin:@172.30.1.58:1521:xe";
+	private String url = "jdbc:oracle:thin:@localhost:1521:xe"; // 테스트용 로컬
+	//private String url = "jdbc:oracle:thin:@172.30.1.58:1521:xe";
 	private String uid = "LINKER";
 	private String upw = "LINKER";
 }
