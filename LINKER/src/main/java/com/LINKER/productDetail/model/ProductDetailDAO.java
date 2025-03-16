1b621c21dfa7a2fc130c634b55238ca432aed001
cypark728 <cypark448@gmail.com>
2025-03-17 01:22:04 +0900

    2025-03-17

--- LINKER/src/main/java/com/LINKER/productDetail/model/ProductDetailDAO.java
+++ LINKER/src/main/java/com/LINKER/productDetail/model/ProductDetailDAO.java
@@ -1,5 +1,12 @@
 package com.LINKER.productDetail.model;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
+
 public class ProductDetailDAO {
 
 	private static ProductDetailDAO instance = new ProductDetailDAO();
@@ -15,4 +22,137 @@ public class ProductDetailDAO {
 	//private String url = "jdbc:oracle:thin:@172.30.1.58:1521:xe";
 	private String uid = "LINKER";
 	private String upw = "LINKER";
+	
+	
+	//제품 상세 글쓰기
+	public void writeProductDetail(ProductDetailDTO dto){
+		
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		
+		String sql = "INSERT INTO PRODUCT_DETAIL (PRODUCT_DETAIL_ID, PRODUCT_IMAGE, PRODUCT_DESCRIPTION,"
+				+ "PRODUCT_CONTACT_INFO, PRODUCT_PRICE, PRODUCT_TYPE, USER_ID) "
+				+ "VALUES(product_detail_seq.NEXTVAL, ?, ?, ?)";
+		
+		try {
+			Class.forName("oracle.jdbc.driver.OracleDriver");
+			conn = DriverManager.getConnection(url, uid, upw);
+			pstmt = conn.prepareStatement(sql);
+			pstmt.setBytes(1, dto.productImage);
+			pstmt.setString(2, dto.productDescription);
+			pstmt.setString(3, dto.productContactInfo);
+			pstmt.setInt(4, dto.productPrice);
+			pstmt.setString(5, dto.productType);
+			pstmt.setInt(6, dto.userID);
+			
+			pstmt.executeUpdate();
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if(conn != null) conn.close();
+				if(pstmt != null) pstmt.close();
+				if(rs != null) rs.close();
+			} catch (Exception e2) {
+			}
+		}
+		
+	}
+	
+	
+	//제품 상세 목록 (전체 반환)
+	public List<ProductDetailDTO> getProductDetail() {
+		//반환용 리스트
+        List<ProductDetailDTO> productDetailList = new ArrayList<>();
+        
+        Connection conn = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+        
+        String sql = "SELECT PRODUCT_DETAIL_ID, PRODUCT_IMAGE, PRODUCT_DESCRIPTION, PRODUCT_CONTACT_INFO," +
+        			 "PRODUCT_CREATED_AT, PRODUCT_PRICE, PRODUCT_TYPE, USER_ID " +
+                     "FROM PRODUCT_DETAIL ORDER BY PRODUCT_CREATED_AT DESC";
+
+        try {
+        	Class.forName("oracle.jdbc.driver.OracleDriver");
+			conn = DriverManager.getConnection(url, uid, upw);
+			pstmt = conn.prepareStatement(sql);
+			
+			rs = pstmt.executeQuery();
+			
+			while (rs.next()) {
+				ProductDetailDTO dto = new ProductDetailDTO();
+                dto.setProductDetailId(rs.getInt("product_detail_id"));
+                dto.setProductImage(rs.getBytes("product_image"));
+                dto.setProductDescription(rs.getString("product_description"));
+                dto.setProductContactInfo(rs.getString("product_contact_info"));
+                dto.setProductCreatedAt(rs.getTimestamp("product_created_at"));
+                dto.setProductPrice(rs.getInt("product_price"));
+                dto.setProductType(rs.getString("product_type"));
+                dto.setUserID(rs.getInt("user_id"));
+                productDetailList.add(dto); 
+            }
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if(conn != null) conn.close();
+				if(pstmt != null) pstmt.close();
+				if(rs != null) rs.close();
+			} catch (Exception e2) {
+			}
+		}
+
+        return productDetailList;
+    }
+	
+	//제품 상세 내용 (제품 아이디 받아서 한개만)
+	public ProductDetailDTO getProductDetailByID(int productDetailId) {
+		
+		//반환용 dto
+		ProductDetailDTO dto = new ProductDetailDTO();
+		
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		
+		String sql = "SELECT PRODUCT_DETAIL_ID, PRODUCT_IMAGE, PRODUCT_DESCRIPTION, PRODUCT_CONTACT_INFO," +
+   			 	"PRODUCT_CREATED_AT, PRODUCT_PRICE, PRODUCT_TYPE, USER_ID " +
+                "FROM PRODUCT_DETAIL WEHRE PRODUCT_DETAIL_ID = ? ORDER BY PRODUCT_CREATED_AT DESC";
+
+		try {
+			Class.forName("oracle.jdbc.driver.OracleDriver");
+			conn = DriverManager.getConnection(url, uid, upw);
+			pstmt = conn.prepareStatement(sql);
+			pstmt.setInt(1, productDetailId);
+			
+			rs = pstmt.executeQuery();
+			
+			if(rs.next()) {
+				dto.setProductDetailId(rs.getInt("product_detail_id"));
+                dto.setProductImage(rs.getBytes("product_image"));
+                dto.setProductDescription(rs.getString("product_description"));
+                dto.setProductContactInfo(rs.getString("product_contact_info"));
+                dto.setProductCreatedAt(rs.getTimestamp("product_created_at"));
+                dto.setProductPrice(rs.getInt("product_price"));
+                dto.setProductType(rs.getString("product_type"));
+                dto.setUserID(rs.getInt("user_id"));
+			}
+			
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if(conn != null) conn.close();
+				if(pstmt != null) pstmt.close();
+				if(rs != null) rs.close();
+			} catch (Exception e2) {
+			}
+		}
+		
+		return dto;
+	}
+	
 }
